c85011a5cf3d1462a9b5da17ec52f8be4abd6b5e
Ali Hussnain <143886682+ahussnain6@users.noreply.github.com>
2024-06-21 08:44:20 +0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,10 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "./components/Home.vue";
-import Cart from "./components/Cart.vue";
-import Details from "./components/Details.vue";
 const routes = [
   {
     name:"Home",
     path: "/",
-    component: Home,
-  },
-  {
-    name:"Cart",
-    path: "/cart",
-    component: Cart,
-  },
-  {
-    name:"Details",
-    path: "/details",
-    component: Details,
-  },
-];
+    component: Home,} ];
 const router = createRouter({
   history: createWebHistory(),
   routes,
